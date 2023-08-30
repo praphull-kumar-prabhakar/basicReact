7daed46cdd3f7d24d97e9f2eda0af703a6f25c7b
praphull-kumar-prabhakar <praphull@rvs.com>
2023-08-30 11:52:47 +0530

    basicReact

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Chai from "./chai"
+function App() {
+const username = " aur code"
+
+  return (
+  <>
+  <Chai/>
+  <h1> Hello , Chai{username}</h1>
+  <p> Dear student</p>
+ 
+  </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+function MyApp(){
+    return (
+        <div>
+        <h1>Custom App !! </h1>
+        </div>
+    
+    )
+        
+}
+/*const reactElement ={
+    type: 'a',
+    props: {
+        href:'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}*/
+
+const anotherElement = (
+    <a href = "https://google.com" target = '_blank'>Visite google</a>
+)
+const anotherUser = "chai aur codeee"
+
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    'click me to visit google',
+   anotherElement
+
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+    reactElement
+)
